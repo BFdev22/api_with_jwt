303a919a10d4d9fd852e7e2ca75e87443bd65830
florian <bremaflorian123@gmail.com>
2024-04-12 15:28:56 +0400

    ajout commentaire

--- tests/router.test.js
+++ tests/router.test.js
@@ -5,15 +5,19 @@ const request  = require('supertest');
 var token = "";
 var idTodo;
 
+// test login
 describe('POST /login', () => {
     it('devrait connecter un utilisateur', async () => {
 
+        // faire la requete pour l'authentification
         const response = await request(app)
             .post('/api/login')
             .send({ name: "flo", password: "flo" });
 
+        // parser le resultat
         const jsonResponse = JSON.parse(response.text);
 
+        // message en fonction du resultat
         if(response.status == 200){
             token = jsonResponse.token;
             console.log("Authentification réussie")
